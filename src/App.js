6d98d54dda82cb77034ccc7b357f260fdeecb3b6
christian r <cjr327@lehigh.edu>
2025-09-11 17:16:23 -0400

    router

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import {
-  BrowserRouter as Router,
+import { HashRouter as Router,
   Route,
   Routes,
   Navigate
