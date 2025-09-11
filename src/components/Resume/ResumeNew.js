2381931e8ff9ce6b5c5a4d9ba251c01795782782
christian r <cjr327@lehigh.edu>
2025-09-11 15:48:20 -0400

    path

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "public/Resume.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
+  const pdfPath = "/Resume.pdf"; // Path to PDF in public folder
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -22,15 +22,16 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={pdfPath} // Use the path
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
+            Download Resume
           </Button>
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+          <Document file={pdfPath} className="d-flex justify-content-center"> // Use the path
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
@@ -38,10 +39,11 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={pdfPath} // Use the path
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
+            Download Resume
           </Button>
         </Row>
       </Container>
