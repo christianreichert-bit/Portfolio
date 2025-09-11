75643059c0d4ca36bd74c0a9035d85c32be3636c
christian r <cjr327@lehigh.edu>
2025-09-11 14:52:18 -0400

    g

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "C:\Users\cjrei\Downloads\resume 2025 August.pdf";
+import pdf from "Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
